ac3d4f7162dc95af1b207b4c8ac245a1a59dc2a0
Parship12 <parshipchowdhury@gmail.com>
2025-04-05 13:29:33 +0530

    All routes Phase 1(Part a)

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const Listing = require('../models/listing.js');
+const Listing = require('./models/listing.js');
+const path = require('path');
+const methodOverride = require('method-override');
+const { name } = require('ejs');
 
 const MONGO_URL = 'mongodb://127.0.0.1:27017/mydbpro1';
 main()
@@ -17,27 +20,61 @@ async function main() {
     await mongoose.connect(MONGO_URL);
 }
 
+app.set('view engine', 'ejs'); // Set EJS as the templating engine
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-encoded data
+app.use(methodOverride('_method')); //The methodOverride middleware allows you to use HTTP verbs like PUT, DELETE, and PATCH in HTML forms, which normally only support GET and POST.
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
-/* app.get('/') sets up a route handler for HTTP GET requests to the root URL.
-When a user accesses the root URL, the callback function (req, res) => {...} executes.
-res.send('Hello World!') sends the text "Hello World!" as the response. */
 
-app.get('/testListing', async (req, res) => {
-    let sampleListing = new Listing({
-        title: "My New Home",
-        description: "By the beach",
-        image: "",
-        price: 1200,
-        location: "New York",
-        country: "USA"
-    });
+//Index route for listings
+app.get("/listings", async(req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", {allListings});
+});
 
-    await sampleListing.save();
-    console.log('Sample listing saved');
-    res.send('successful testing');
-})
+//new route for listings
+app.get("/listings/new", (req, res) => {
+    res.render("listings/new.ejs");
+});
+
+//show route for listings
+app.get("/listings/:id", async(req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", {listing});
+});
+
+//create route for listings
+app.post("/listings", async(req, res) => {
+    const newListing = new Listing(req.body.listing); // Create a new listing using the data from the form
+    await newListing.save(); // Save the new listing to the database
+    res.redirect("/listings"); // Redirect to the index page after saving
+});
+
+//edit route for listings
+app.get("/listings/:id/edit", async(req, res) => {
+    let { id } = req.params; // Get the ID from the request parameters
+    const listing = await Listing.findById(id); // Find the listing by ID in the database
+    res.render("listings/edit.ejs", {listing});
+});
+
+//update route for listings
+app.put("/listings/:id", async(req, res) => {
+    let { id } = req.params; // Get the ID from the request parameters
+    await Listing.findByIdAndUpdate(id,{...req.body.listing}); // Update the listing in the database
+    res.redirect(`/listings/${id}`); // Redirect to the show page of the updated listing
+});
+
+//delete route for listings
+app.delete("/listings/:id", async(req, res) => {
+    let { id } = req.params; // Get the ID from the request parameters
+    let deletedListing = await Listing.findByIdAndDelete(id); // Delete the listing from the database
+    res.redirect("/listings"); // Redirect to the index page after deletion
+    console.log("Deleted this listing:", deletedListing.title); // Log the ID of the deleted listing
+});
 
 app.listen(8080, () => { // Start the server on port 8080
   console.log('Server is running on port 8080');
